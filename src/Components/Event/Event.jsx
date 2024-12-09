9ae54fc61294469fb126041096265088175d2592
DESKTOP-VM6EIJB\Imashi Thakshila <imashi-php@webappclouds.com>
2024-12-09 12:18:23 +0530

    nav_bar

--- src/Components/Event/Event.jsx
+++ src/Components/Event/Event.jsx
@@ -10,8 +10,8 @@ const Event = () => {
                 <ul>
                     <li><a href="/home">Home</a></li>
                     <li><a href="news.asp">News</a></li>
-                    <li><a href="/contact">Contact Us</a></li>
-                    <li><a href="/about">About</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/about">About Us</a></li>
                 </ul>
             </div>
         <div className="event-container">
