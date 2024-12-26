75c428b9621c041f7fc7cabb4a09d171a45970d1
DESKTOP-VM6EIJB\Imashi Thakshila <imashi-php@webappclouds.com>
2024-12-26 11:49:28 +0530

    section

--- src/Components/Section/Section.jsx
+++ src/Components/Section/Section.jsx
@@ -2,8 +2,22 @@ import { Link } from 'react-router-dom';
 import React from "react";
 import './Section.css';
 const Section= () => {
-    <>
-    </>
+    return (
+        <>
+        <div className='section'>
+            <div className='nav-bar'>
+                <div className='logo'></div>
+                {/* <img src="./Components/Assests/DineFlow.png" alt="Logo" className="nav-logo" /> */}
+                <ul>
+                    <li><a href="/home">Home</a></li>
+                    <li><a href="news.asp">News</a></li>
+                    <li><a href="/contact">Contact</a></li>
+                    <li><a href="/about">About Us</a></li>
+                </ul>
+            </div>
+        </div>        
+        </>
+    );
 };
 
 export default Section;
